5c91c65bb437d49a45eb33035594a33c7e4c582e
Florent Teissier <teissierflorent@gmail.com>
2023-02-18 15:34:40 +0100

    add jest config

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  testMatch: ["**/__tests__/**/*.test.ts?(x)"],
+  transform: {
+    "^.+\\.(t|j)sx?$": "@swc/jest",
+  }
+};
---
+++ src/__tests__/utils.test.ts
@@ -0,0 +1,7 @@
+import { add } from "../utils";
+
+describe("test", () => {
+  it("should be true", () => {
+    expect(add(2, 3)).toBe(5);
+  });
+});
